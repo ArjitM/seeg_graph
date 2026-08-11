9bff022bb26b6209c38114a7339d2079a1634419
Arjit Misra <arjit@Mac.lan>
2026-08-11 01:33:52 -0500

    control centrality

--- graph_scaffold.py
+++ graph_scaffold.py
@@ -1,21 +1,38 @@
+"""
+Code to create a Graph representation for 2D data, designed for brain structural and functional/effective connectivity
+explorations.
+"""
+__author__ = "Arjit Misra"
+__email__ = ["arjitm@uchicago.edu", "arjitm2@illinois.edu"]
+__version__ = "2026-Aug-11"
+
 import networkx as nx
 import numpy as np
+import numpy.typing as npt
 
 
-
-def matToGraph(matrix2d, node_labels, node_val_dict) -> nx.Graph:
+def matToGraph(matrix2d, node_labels: npt.ArrayLike, node_val_dict=None) -> nx.Graph:
+    """
+    Create a Graph from a 2D matrix, representing the pairwise Adjacency Matrix.
+    :param matrix2d: Adjacency matrix
+    :param node_labels: Node identities
+    :param node_val_dict:
+    :return:
+    """
     G = nx.Graph()
     if np.all((matrix2d - matrix2d.T)!=0):
+        # Make symmetric iff matrix is not already symmetric
         matrix2d = matrix2d + matrix2d.T  # assume matrix is lower or upper triangular with 0 diagonal
 
     inds = list(zip(*np.triu_indices(matrix2d.shape[0], 1)))  # [ (i1, j1), (i2, j2), ... ]
     weights = [matrix2d[ii] for ii in inds]
-    ind_pairs = zip(*inds)
     node_pairs = [(node_labels[i], node_labels[j]) for (i, j) in inds]
 
-    G.add_weighted_edges_from(zip(*zip(node_pairs), weights))
-    for nl in node_labels:
-        G.add_nodes_from((nl, node_val_dict.get(nl)))
+    G.add_weighted_edges_from(zip(*zip(node_pairs), weights))  #  < (node_1, node_2, weight) ... >
+
+    if node_val_dict is not None:  # Optionally assign node attributes (may be multiple)
+        for nl in node_labels:
+            G.add_nodes_from((nl, node_val_dict.get(nl)))
 
     return G
 
@@ -38,13 +55,15 @@ def centrality(G: nx.Graph, directed=False):
         return in_c, out_c
 
 
-def smoothness(G: nx.Graph, x: np.array, normalized=True, symm=False):
-
+def smoothness(G: nx.Graph, x: npt.ArrayLike, normalized=True, symm=True):
+    x = np.array(x)
     if not symm:
+        # L = D - A
         L = nx.laplacian_matrix(G).toarray()
         D = L.diagonal() * np.identity(L.shape[0])
         norm = x.T @ D @ x
     else:
+        # L_symm = I - D^(-1/2) A D^(-1/2)
         L = nx.normalized_laplacian_matrix(G).toarray()  # symmetrically normalized
         norm = x.T @ x
 
@@ -63,3 +82,50 @@ def synchronizability(G: nx.Graph):
     return eigenvals[1] / eigenvals[-1]
 
 
+def _substitute_node(G: nx.Graph, node, scaffold: nx.Graph) -> nx.Graph:
+    """
+    Helper method to substitute node edge weights with
+    those of a control/normative scaffold graphD.
+    :param G:
+    :param node:
+    :param scaffold: normative Graph or corresponding Adjacency matrix.
+    :return:
+    """
+    G_new = G.copy()
+    G_new.remove_edges_from(list(G.edges(node)))
+    G_new.add_weighted_edges_from(scaffold.edges(node))
+    return G_new
+
+
+def control_centrality_resection(G: nx.Graph, nodes: npt.ArrayLike):
+    """
+    Calculates the change in network sychronizability upon removal of graph node NODE.
+    Introduced by Khambati et al, 2016. Under this framework, large positive (Z-transformed) increases in network
+    synchronizability upon node resection implies the node is a network desynchronizer. Conversely, large
+    decreases in synchronizability upon resection implies the node is a network synchronizer.
+    """
+    cc = []
+    cc_0 = synchronizability(G)
+
+    for node in nodes:
+        G_c = G.copy()
+        G_c.remove_node(node)
+        cc.append(synchronizability(G_c) - cc_0)
+
+    return cc
+
+
+def control_centrality_transplant(G_0: nx.Graph, G_normative: nx.Graph, nodes: npt.ArrayLike):
+    """
+    Our novel extension of Khambati et al. framework. Instead of node removal, we substitute node edge weights with
+    those of a control/normative graph, G_normative and calculate the change in network
+    synchronizabilty upon doing so.
+    """
+    cc = []
+    cc_0 = synchronizability(G_0)
+
+    for node in nodes:
+        G_sub = _substitute_node(G_0, G_normative, node)
+        cc.append(synchronizability(G_sub) - cc_0)
+
+    return cc
